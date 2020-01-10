d3be8792ef4b4f3a153bc928745da8cd8f7b7c91
Alex Wang <idea.wangtj@gmail.com>
2020-01-11 06:22:19 +0800

    第三课作业: save status to gaia and render

--- src/Profile.js
+++ src/Profile.js
@@ -17,8 +17,65 @@ export default class Profile extends Component {
   	  	avatarUrl() {
   	  	  return avatarFallbackImage;
   	  	},
-  	  },
+      },
+      newStatus: "",
+      status: ""
   	};
+  }
+
+  //read data from gaia
+  fetchData() {
+    const { userSession } = this.props
+    const options = { decrypt: false }
+    userSession.getFile('status.json', options)
+      .then((file) => {
+        var status = JSON.parse(file || '[]')
+        console.log(status)
+        this.setState({
+          status:status
+        })
+      })
+      .finally(() => {
+        console.log("read over")
+      })
+   }
+
+  // handle event for input change
+  handleNewStatusChange(event){
+    this.setState({
+      newStatus: event.target.value
+    })
+  }
+
+  //handle event for button submit
+  handleNewStatusSubmit(event){
+    this.saveNewStatus(this.state.newStatus)
+    this.setState({
+      newStatus: ""
+    })
+  }
+
+  //save data to gaia
+  saveNewStatus(statusText){
+    //01 get userSession
+    const {userSession} = this.props
+
+    //02 data will be saved
+    let gaia_status = {
+      text: statusText.trim(),
+      create_at: Date.now()
+    }
+
+    //03 parameter options without encrypt
+    const options = { encrypt: false }
+
+    //04 invoke gaia interface to save data
+    userSession.putFile('status.json', JSON.stringify(gaia_status), options)
+    .then(() => {
+      this.setState({
+        newStatus: gaia_status.text
+      })
+    })
   }
 
   render() {
@@ -31,7 +88,7 @@ export default class Profile extends Component {
         <div className="avatar-section">
           <img src={ person.avatarUrl() ? person.avatarUrl() : avatarFallbackImage } className="img-rounded avatar" id="avatar-image" alt=""/>
         </div>
-        <h1>Hello, <span id="heading-name">{ person.name() ? person.name() : 'Nameless Person' }</span>!</h1>
+        <p>Hello, <span id="heading-name">{ person.name() ? person.name() : 'Nameless Person' }</span>!</p>
         <p className="lead">
           <button
             className="btn btn-primary btn-lg"
@@ -41,10 +98,25 @@ export default class Profile extends Component {
             Logout
           </button>
         </p>
+        <textarea className="input-status"
+          value={this.state.newStatus}
+          onChange={e=> this.handleNewStatusChange(e)}
+          placeholder="输入状态" />
+          <br/>
+          <button
+            className="btn btn-primary btn-lg"
+            onClick={e=> this.handleNewStatusSubmit(e)}
+          >
+            提交
+          </button>
+          <p>  status is: {this.state.status.text}</p>
       </div> : null
     );
   }
 
+  componentDidMount() {
+    this.fetchData()
+  }
   componentWillMount() {
     const { userSession } = this.props;
     this.setState({
